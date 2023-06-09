036f4fa1aa37672711701a3dc62cc39e06ab377c
s1300163 <s1300163@std4dc31>
2023-06-09 11:35:38 +0900

    Dice game: initial revision

---
+++ dice.py
@@ -0,0 +1,11 @@
+import random
+
+print("Rolling dice...")
+
+num1 = random.randint(1, 6)
+print("Die 1:",num1)
+num2 = random.randint(1, 6)
+print("Die 2:",num2)
+
+sum = num1 + num2
+print("Total value:",sum)
